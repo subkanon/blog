800d656e548475d7e205526883ec15e2594abfd0
subnetcannon <dave8205@yahoo.com>
2020-09-28 18:06:00 -0500

    Heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = BASE_DIR,'static'
-STATIC_ROOT = STATIC_ROOT = BASE_DIR,'staticfiles')
+STATIC_ROOT = STATIC_ROOT = BASE_DIR,'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
